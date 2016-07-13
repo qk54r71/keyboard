30a83803d3be27f87a107a3f8a9a5eb48525613a
pjb <qk54r71@nate.com>
2016-07-13 19:48:05 +0900

    화면 재구성 및 기능 재구현
    키보드 xml 을 Layout 구성으로 변경

--- app/src/main/java/com/example/user/myapplication/CustomKoreanKeyboard.java
+++ app/src/main/java/com/example/user/myapplication/CustomKoreanKeyboard.java
@@ -1,138 +1,128 @@
 package com.example.user.myapplication;
 
+import android.content.Context;
 import android.inputmethodservice.InputMethodService;
-import android.inputmethodservice.Keyboard;
-import android.inputmethodservice.KeyboardView;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import android.widget.Button;
 
 /**
  * Created by USER on 2016-07-11.
  */
-public class CustomKoreanKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+public class CustomKoreanKeyboard extends InputMethodService implements View.OnClickListener {
 
-    private KeyboardView kv;
-    private Keyboard keyboard;
-    /**
-     * 단계별로 처리하기 위한 변수
-     * mDepth == 0 자음
-     * mDepth == 1 자음+모음
-     * mDepth == 2 자음+모음+받침
-     */
-    private Integer mDepth = 0;
-    /**
-     * 단계중 자음 화면
-     */
-    private final int DEPTH_FIRST = 0;
-    /**
-     * 단계중 자음 + 모음 화면
-     */
-    private final int DEPTH_SECOND = 1;
-    /**
-     * 단계중 자음 + 모음 + 받침 화면
-     */
-    private final int DEPTH_THIRD = 2;
-    /**
-     * 공백값 코드
-     */
-    private final int KEYCODE_SPACE = -7;
+    private Button[] mButton = new Button[25];
+
+    private final String KEYBOARD_DELETE = "삭제";
+    private final String KEYBOARD_SPACE = "공백";
+    private final String KETBOARD_DONE = "엔터";
 
     /**
-     * 구성된 키보드 화면 매칭
+     * TableLayout 을 사용한 CustomKeyboard 화면을 Match 시킨다.
      *
-     * @return 매칭된 키보드
+     * @return Custom된 화면
      */
     @Override
     public View onCreateInputView() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.key, null);
 
-        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
-        keyboard = new Keyboard(CustomKoreanKeyboard.this, R.xml.qwerty_base);
-        kv.setKeyboard(keyboard);
-        kv.setOnKeyboardActionListener(CustomKoreanKeyboard.this);
+        setFindView(view);
+        setOnClick();
 
-        return kv;
-    }
-
-    @Override
-    public void onPress(int primaryCode) {
-
-    }
-
-    @Override
-    public void onRelease(int primaryCode) {
-
+        return view;
     }
 
     /**
-     * 화면에서 눌리는 키 이벤트 처리
+     * Custom 된 화면의 Button 을 Match
      *
-     * @param primaryCode : xml에 명시된 code 값
-     * @param keyCodes    : xml에 명시된 code 값을 배열에 담음
+     * @param view : Current View
+     */
+    private void setFindView(View view) {
+        mButton[0] = (Button) view.findViewById(R.id.button1);
+        mButton[1] = (Button) view.findViewById(R.id.button2);
+        mButton[2] = (Button) view.findViewById(R.id.button3);
+        mButton[3] = (Button) view.findViewById(R.id.button4);
+        mButton[4] = (Button) view.findViewById(R.id.button5);
+        mButton[5] = (Button) view.findViewById(R.id.button6);
+        mButton[6] = (Button) view.findViewById(R.id.button7);
+        mButton[7] = (Button) view.findViewById(R.id.button8);
+        mButton[8] = (Button) view.findViewById(R.id.button9);
+        mButton[9] = (Button) view.findViewById(R.id.button10);
+        mButton[10] = (Button) view.findViewById(R.id.button11);
+        mButton[11] = (Button) view.findViewById(R.id.button12);
+        mButton[12] = (Button) view.findViewById(R.id.button13);
+        mButton[13] = (Button) view.findViewById(R.id.button14);
+        mButton[14] = (Button) view.findViewById(R.id.button15);
+        mButton[15] = (Button) view.findViewById(R.id.button16);
+        mButton[16] = (Button) view.findViewById(R.id.button17);
+        mButton[17] = (Button) view.findViewById(R.id.button18);
+        mButton[18] = (Button) view.findViewById(R.id.button19);
+        mButton[19] = (Button) view.findViewById(R.id.button20);
+        mButton[20] = (Button) view.findViewById(R.id.button21);
+        mButton[21] = (Button) view.findViewById(R.id.button22);
+        mButton[22] = (Button) view.findViewById(R.id.button23);
+        mButton[23] = (Button) view.findViewById(R.id.button24);
+        mButton[24] = (Button) view.findViewById(R.id.button25);
+    }
+
+    /**
+     * Button 에 Click 설정
+     */
+    private void setOnClick() {
+        for (Button button : mButton) {
+            button.setOnClickListener(this);
+        }
+    }
+
+    /**
+     * Click Event 설정
+     *
+     * @param view : Current View
      */
     @Override
-    public void onKey(int primaryCode, int[] keyCodes) {
-
-        Log.i("CustomKey", "primaryCode : " + primaryCode);
+    public void onClick(View view) {
+        Log.i("CustomKey", "View : " + view);
 
         InputConnection ic = getCurrentInputConnection();
 
-        switch (primaryCode) {
-            case Keyboard.KEYCODE_DELETE:
+        String strSwitch = (String) ((Button) view).getText();
+
+        switch (strSwitch) {
+            case KEYBOARD_DELETE:
                 ic.deleteSurroundingText(1, 0);
                 break;
-            case Keyboard.KEYCODE_DONE:
+            case KETBOARD_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
-            case KEYCODE_SPACE:
+            case KEYBOARD_SPACE:
                 ic.commitText(" ", 1);
                 break;
             default:
-                char code = (char) primaryCode;
+                Log.i("CustomKey", "Test : " + ((Button) view).getText());
 
-                Log.i("CustomKey", "mDepth : " + mDepth);
+                CharSequence charText = ((Button) view).getText();
 
-                switch (mDepth) {
-                    case DEPTH_FIRST:
-                        ic.commitText(String.valueOf(code), 1);
-                        mDepth++;
-                        break;
-                    case DEPTH_SECOND:
-                        ic.deleteSurroundingText(1, 0);
-                        ic.commitText(String.valueOf(code), 1);
-                        mDepth++;
-                        break;
-                    case DEPTH_THIRD:
-                        mDepth = DEPTH_FIRST;
-                        break;
-                }
+                ic.commitText(String.valueOf(charText.charAt(0)), 1);
+
+
+
+
         }
-
     }
 
-    @Override
-    public void onText(CharSequence text) {
+    /**
+     * 눌리는 버튼에 따라서 키보드의 텍스트 값 변경
+     *
+     * @param strButton :  Click Button Text
+     */
+    private void setButtonText(String strButton) {
+        switch(){
 
+        }
     }
 
-    @Override
-    public void swipeLeft() {
-
-    }
-
-    @Override
-    public void swipeRight() {
-
-    }
-
-    @Override
-    public void swipeDown() {
-
-    }
-
-    @Override
-    public void swipeUp() {
-
-    }
 }
